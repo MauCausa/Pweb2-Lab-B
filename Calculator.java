03418cf32d0e9eed45993f61cbbfbb11cbd5eb18
Sebastianzzzin <120695582+Sebastianzzzin@users.noreply.github.com>
2023-05-16 00:06:36 -0500

    Update Calculator.java
    creacion del método mul

--- Calculator.java
+++ Calculator.java
@@ -28,6 +28,9 @@ public class Calculator {
      }
      public int sub(int first,int second){
         return first-second;
-     } 
+     }
+     public int mul(int first,int second){
+         return first*second;
+     }
     }
 }
