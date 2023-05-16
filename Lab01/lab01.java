5499f97831b4b041a0d0265b2ab5864f20b1c0b3
MauCausa <121000971+MauCausa@users.noreply.github.com>
2023-05-15 23:04:49 -0500

    Update lab01.java
    Add

--- Lab01/lab01.java
+++ Lab01/lab01.java
@@ -1,5 +1,12 @@
-public class lab01{
+public class Calculator{
   public static void main(String args[] ){
     System.out.println("Hola Sebas...");
+    System.out.println("Alias 'Mas capito'");
+    
+  }
+  public int add(int a, int b){
+    int suma=0;
+    suma=a+b;
+    return suma;
   }
 }
