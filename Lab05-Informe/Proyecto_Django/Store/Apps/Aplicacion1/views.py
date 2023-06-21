7d4ddbb9447c6630ee8a4c5dc9aad00561450618
MaxsForocca <mforocca@unsa.edu.pe>
2023-06-21 00:11:27 -0500

    index

--- Lab05-Informe/Proyecto_Django/Store/Apps/Aplicacion1/views.py
+++ Lab05-Informe/Proyecto_Django/Store/Apps/Aplicacion1/views.py
@@ -1,11 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import Template, Context
 # Create your views here.
-def index(response):
-    doc_externo = open("./templates/index.html")
-    plt = Template(doc_externo.read())
-    doc_externo.close()
-    ctx = Context()
-    document = plt.render(ctx)
-    return HttpResponse(document)
+def index(request):
+    return render(request, "index.html", {})
