3e238047b7c2010747eb2401cb4fbbf69d423818
MaxsForocca <mforocca@unsa.edu.pe>
2023-06-20 23:48:44 -0500

    dir templates

--- Lab05-Informe/Proyecto_Django/Store/Apps/Aplicacion1/views.py
+++ Lab05-Informe/Proyecto_Django/Store/Apps/Aplicacion1/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import Template, Context
 # Create your views here.
+def index(response):
+    doc_externo = open("./templates/index.html")
+    plt = Template(doc_externo.read())
+    doc_externo.close()
+    ctx = Context()
+    document = plt.render(ctx)
+    return HttpResponse(document)
--- Lab05-Informe/Proyecto_Django/Store/Store/urls.py
+++ Lab05-Informe/Proyecto_Django/Store/Store/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from Apps.Aplicacion1 import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', views.index)
 ]
