ed09c667d6b31663d52a71677ed15cc5a8c7e5ee
MaxsForocca <mforocca@unsa.edu.pe>
2023-07-02 16:25:53 -0500

    modificando models de aplicacion1

--- Lab06-Informe/project/aplicacion1/models.py
+++ Lab06-Informe/project/aplicacion1/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+"""
+class DestinosTuristicos(models.Model):
+    nombreCuidad = models.CharField(max_length=150, blank=True)
+    descripcionCiudad = models.CharField(max_length=400, blank=True)
+    imagenCiudad = models.ImageField(blank=True, null=True)
+    precioTour = models.FloatField
+    ofertaTour = models.BooleanField
+"""
