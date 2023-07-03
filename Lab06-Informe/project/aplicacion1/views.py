f03177531558d12bc88b6b029cedafe8b771e643
MaxsForocca <mforocca@unsa.edu.pe>
2023-07-02 19:27:18 -0500

    templates post

--- Lab06-Informe/project/aplicacion1/views.py
+++ Lab06-Informe/project/aplicacion1/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>HOLA MUNDO</h1>")
+    return render(request, 'home.html', {'name':'Mundo'})
+
+def add(request):
+    
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
+    
+    res= val1+val2
+    
+    return render(request, 'result.html', {'result': res})
