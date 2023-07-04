26760980393241bb67d6b0954c420d707cf2296d
MaxsForocca <mforocca@unsa.edu.pe>
2023-07-03 21:06:17 -0500

    modicando models

--- Lab06-Informe/project/travello/models.py
+++ Lab06-Informe/project/travello/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+"""
+class DestinosTuristicos(models.Model):
+    nombreCuidad = models.CharField(max_length=150, blank=True)
+    descripcionCiudad = models.CharField(max_length=400, blank=True)
+    imagenCiudad = models.ImageField(blank=True, null=True)
+    precioTour = models.FloatField
+    ofertaTour = models.BooleanField
+"""
+"""
+???
+class Usuario(models.Model):
+    nombres = models.CharField(max_length=150, blank=True)
+    apellidos = models.CharField(max_length=200, blank=True)
+    username = models.CharField(max_length=150)
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
+
+"""
