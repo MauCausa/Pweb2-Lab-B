cde77d65ff69b12955aff943073e798eb1648dae
Klismann30 <118670708+klismannSis@users.noreply.github.com>
2023-07-25 23:03:56 -0500

    Ready

--- Lab08-Informe/proyecto/aplicacion/urls.py
+++ Lab08-Informe/proyecto/aplicacion/urls.py
@@ -4,9 +4,13 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register(r'usuario', views.UsuarioViewSet)
+router.register(r'BODEGA', views.BodegaViewSet)
+
 
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('home/', views.home , name='home'),
+    path('', include(router.urls))
+    
 ]
