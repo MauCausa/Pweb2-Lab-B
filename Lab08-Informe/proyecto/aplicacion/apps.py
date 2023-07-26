42aca74c974ce41deb5a55f746360bd8a6a786b1
Klismann30 <118670708+klismannSis@users.noreply.github.com>
2023-07-25 23:00:58 -0500

    Class apiConfig

--- Lab08-Informe/proyecto/aplicacion/apps.py
+++ Lab08-Informe/proyecto/aplicacion/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class AplicacionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'aplicacion'
+    
+class ApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'api'
