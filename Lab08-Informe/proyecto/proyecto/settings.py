48f27310ad0bdfe39c97d6ed3456d4dd403c008e
Klismann30 <118670708+klismannSis@users.noreply.github.com>
2023-07-25 23:06:20 -0500

    Completo...

--- Lab08-Informe/proyecto/proyecto/settings.py
+++ Lab08-Informe/proyecto/proyecto/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'aplicacion',
+    'coreapi', 
+    'api'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+REST_FRAMEWORK = {
+  'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
+}
+
