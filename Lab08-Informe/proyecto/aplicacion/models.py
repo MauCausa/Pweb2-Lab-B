c8799b3ca6c3f36195dca421df69fde7037cf93d
Klismann30 <118670708+klismannSis@users.noreply.github.com>
2023-07-25 23:01:49 -0500

    new class

--- Lab08-Informe/proyecto/aplicacion/models.py
+++ Lab08-Informe/proyecto/aplicacion/models.py
@@ -8,3 +8,9 @@ class Usuario(models.Model):
     nombres = models.CharField(max_length= 200)
     apellidos = models.CharField(max_length= 150)
     isPremium = models.BooleanField(default=False)
+
+class Bodega(models.Model):
+    Customer_Name = models.CharField(max_length=100)
+    Product = models.CharField(max_length=50)
+    Price =  models.PositiveSmallIntegerField()
+    is_activate = models.BooleanField(default=True)
